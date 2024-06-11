6b72358f37503c0a0d7901ac933e18f11e0b287c
ramadhanveha <ramadhanveha@gmail.com>
2024-06-11 10:53:20 +0700

    pembaruhan

---
+++ myDockerfile/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		fmt.Println("Someone hit me!")
+		return c.HTML(http.StatusOK, "Returning...")
+	})
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
