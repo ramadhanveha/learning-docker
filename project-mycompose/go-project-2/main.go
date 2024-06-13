88cd32b08c27413094bf23e967d788bf544af0c1
ramadhanveha <127192268+ramadhanveha@users.noreply.github.com>
2024-06-13 22:03:55 +0700

    Add files via upload

---
+++ project-mycompose/go-project-2/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+
+	// get env
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	// connect database
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPass, dbHost, dbPort, dbName)
+	fmt.Println("Connect to: ", connStr)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Connection to database failed: ", err)
+	}
+
+	//tes ping to database
+	if err := db.Ping(); err != nil {
+		fmt.Println("Ping to database failed (DB_HOST: %s): %s\n", dbHost, err)
+	} else {
+		fmt.Println("Successfully connected to DB : ", db)
+	}
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.POST("/ping", func(c echo.Context) error {
+		query := `INSERT INTO veha_access_log (timestamp) VALUES (NOW())`
+		_, err := db.Exec(query)
+		if err != nil {
+			fmt.Printf("ping failed: \n")
+			return c.HTML(http.StatusInternalServerError, "ouch")
+		} else {
+			fmt.Println("ping successful")
+			return c.HTML(http.StatusOK, "pong")
+		}
+	})
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "77"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
